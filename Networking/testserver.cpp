09921a5c8bf0764c2ebcda3e14b972177056092a
Rohit Yadav <147850414+YADAV1825@users.noreply.github.com>
2025-06-22 21:12:02 +0530

    Update testserver.cpp

--- Networking/testserver.cpp
+++ Networking/testserver.cpp
@@ -6,7 +6,7 @@
     DATE: 22nd June,2025
 */
 
-#include <thread>
+
 #include "testserver.hpp"
 using namespace std;
 
@@ -61,35 +61,11 @@ void HDE::testserver::launch()
 {
     while (true)
     {
-        cout << "---waiting---" << endl;
-        accepter();  // Accept connection, store new_socket
-
-        std::thread client_thread(&HDE::testserver::handle_client, this, new_socket);
-        client_thread.detach(); // Run in background
+        cout<<"---waiting---"<<endl;
+        accepter();
+        handler();
+        responder();
+        cout<<"---Done---"<<endl;
     }
-}
-
-//handle client ( i added this later to make the server multi-thread from single thread)~Rohit
-void HDE::testserver::handle_client(int client_socket)
-{
-    char local_buffer[30000] = {0};
     
-    // Read request
-    read(client_socket, local_buffer, sizeof(local_buffer));
-    cout << local_buffer << endl;
-
-    // Prepare response
-    const char *body = "<h1>Hello from Rohit's Multi-threaded Server!</h1>";
-    std::string response = 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "Content-Length: " + std::to_string(strlen(body)) + "\r\n"
-        "Connection: close\r\n"
-        "\r\n" + std::string(body);
-
-    // Send response
-    write(client_socket, response.c_str(), response.length());
-    close(client_socket);
-
-    cout << "---Done---" << endl;
 }
